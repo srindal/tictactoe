42784ff3522db8983cb4e65750eb66c0b47d87ab
srindal <rindal@gmail.com>
2017-04-24 23:41:39 +0200

    Added webpack support

---
+++ scripts/tictac.js
@@ -0,0 +1,32 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+
+var Cell = React.createClass({
+    render: function() {
+        var colors = ["white", "red", "yellow"];
+        return <div style={{backgroundColor:colors[this.props.itm]}} className="cell">
+            {this.props.itm}
+            </div>
+    }
+});
+
+var TicTacToe = React.createClass({
+  componentWillMount: function() {this.setState({game:[0,0,0,0,0,0,0,0,0], player: 1})},
+  render: function() { 
+    var rows = [];
+    this.state.game.forEach(function(itm, idx) {
+        rows.push(<Cell itm={itm} key={idx}/>);
+    }, this);
+    return (
+      <div>
+        TicTacToe - Player {this.state.player} to move
+        <div className="board">{rows}</div>
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<TicTacToe/>, document.getElementById('content'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'browser-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+    ]
+  }
+};
